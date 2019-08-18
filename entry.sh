7cb953ea673743c2a6ae9cc3980b89e38a741bc9
Yegor Bugayenko <yegor256@gmail.com>
2019-08-18 20:34:00 +0300

    bundle update

--- entry.sh
+++ entry.sh
@@ -15,9 +15,11 @@ fi
 if [ -d 0dmx ]; then
   cd 0dmx
   git pull
-  cd ..
 else
   git clone "https://yegor256:${GITHUB_TOKEN}@github.com/yegor256/0dmx"
+  cd 0dmx
 fi
 
-./0dmx/bin/brigade "--secret=${SECRET}"
+bundle update
+
+./bin/brigade "--secret=${SECRET}"
